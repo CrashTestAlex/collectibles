c034881c8a1f6c616e8b3a73de501cadd53154b2
CrashTestAlex <alexkola109@icloud.com>
2025-12-20 22:29:20 -0500

    Update apps.py

--- collect/collect/apps.py
+++ collect/collect/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
-class YourAppConfig(AppConfig):
+
+class CollectConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "collect"
+    label = "collect"
+    verbose_name = "Collectibles"
+
     dpy_package = "collect.package"
